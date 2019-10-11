826d8e8b282c0f0053c2bd3174c618118c73c177
jeyofdev <jgregoire.pro@gmail.com>
2019-10-11 23:29:28 +0200

    remove the public folder from .gitignore

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>webpack</title>
+        <link rel="stylesheet" href="http://localhost:8080/assets/css/app.css">
+    </head>
+
+    <body>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis expedita nam excepturi ab nemo adipisci doloribus quae aspernatur earum quo!</p>
+        <script src="http://localhost:8080/assets/js/app.js"></script>
+    </body>
+</html>
