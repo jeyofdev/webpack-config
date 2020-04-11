b3984038aa0ba3e48d34e429cf30ba5e5848cb57
jeyofdev <jgregoire.pro@gmail.com>
2020-04-11 16:02:04 +0200

    add bootstrap

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/styles/app.css">
     </head>
 
@@ -33,7 +32,11 @@
                 <div class="container">
                     <h1 class="display-3 text-white">Hello, world!</h1>
                     <p class="text-white">This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-                    <p><a class="btn btn-light btn-lg" href="#" role="button">Learn more »</a></p>
+                    <p>
+                        <a class="btn btn-light btn-lg" href="#" role="button" data-toggle="modal" data-target="#exampleModal">
+                            <i class="fas fa-arrow-alt-circle-right"></i> Learn more »
+                        </a>
+                    </p>
                 </div>
             </div>
 
@@ -58,10 +61,32 @@
                     </div>
                 </div>
             </div>
+            
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, laudantium?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+                </div>
+            </div>
         </main>
 
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <script src="assets/scripts/app.js"></script>
     </body>
 </html>
